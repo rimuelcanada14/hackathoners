aef74cbe63aa977f352d5a148f07d44cd3e3cd19
rimuelcanada14 <102201493+rimuelcanada14@users.noreply.github.com>
2025-01-12 04:58:04 +0800

    fixed center for login anbd signup

--- hackathon/src/Components/Login.jsx
+++ hackathon/src/Components/Login.jsx
@@ -25,31 +25,34 @@ const Login = () => {
   }
   return (  
     <>
-    <div className="login-bg">
-      <img src="./img/waves.png" alt="Background" />
-    </div>
-    <div className="container-fluid login-container">
-      <div className="login-content">
-        <div className="login-logo">
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="./img/logo.png" alt="Background" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <div className = "login-form">
-          {message && <p>{message}</p>}
-            <form onSubmit={handleLogin}>
-              <label>Email: </label>
-              <input id="email" type="email" value={email} onChange={(e) =>setEmail(e.target.value)} />
-              <label>Password: </label>
-              <input id="password" type="password" value={password} onChange={(e) =>setPassword(e.target.value)} />
-              <div className="login-submit">
-                <button type='submit'>LOGIN</button>
-              </div>
-            </form>
-        </div>
-        <div className="login-register">
-          <p className = "subtext">Don't have an account? <Link to="/signup" className='login-reg'>Register</Link></p>
+    <body className="login-page">
+      <div className="login-bg">
+        <img src="./img/waves.png" alt="Background" />
+      </div>
+      <div className="container-fluid login-container">
+        <div className="login-content">
+          <div className="login-logo">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="./img/logo.png" alt="Background" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </div>
+          <div className = "login-form">
+            {message && <p>{message}</p>}
+              <form onSubmit={handleLogin}>
+                <label>Email: </label>
+                <input id="email" type="email" value={email} onChange={(e) =>setEmail(e.target.value)} />
+                <label>Password: </label>
+                <input id="password" type="password" value={password} onChange={(e) =>setPassword(e.target.value)} />
+                <div className="login-submit">
+                  <button type='submit'>LOGIN</button>
+                </div>
+              </form>
+          </div>
+          <div className="login-register">
+            <p className = "subtext">Don't have an account? <Link to="/signup" className='login-reg'>Register</Link></p>
+          </div>
         </div>
       </div>
-    </div>
+    </body>
+    
     </>
     
   )
